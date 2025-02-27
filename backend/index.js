f69b34881234fd7e738b7ad2be20ebc9bc2eb919
siddiquesk <mehnajk07@gmail.com>
2025-02-27 20:58:43 +0530

    final code

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const crudRoutes = require("./routes/crudRoutes");
 const dontenv=require("dotenv");
+const path=require("path");
 dontenv.config();
 const app = express();
 const DB_URL=process.env.MONGO_URL
@@ -21,7 +22,13 @@ mongoose
   .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/chat",crudRoutes);
-
+if(process.env.NODE_ENV==="production"){
+  const dirPath=path.resolve();
+  app.use(express.static("frontend/dist"));
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(dirPath,"frontend","index.html"))
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
